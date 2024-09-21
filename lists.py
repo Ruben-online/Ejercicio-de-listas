dcdca791a3c824e3e96ff95a84067d0614ab2d83
Ruben-online <78996839+Ruben-online@users.noreply.github.com>
2024-09-20 19:46:32 -0600

    Hice el código de nuevo para que archivos .txt sean leidos para posteriormente verificar que fruta o verdura esta o no en las listas

--- lists.py
+++ lists.py
@@ -1,12 +1,19 @@
-fruit_list = ["Manzana", "Banano", "Pera", "Uva"]
-vegetable_list = ["Cebolla", "Ajo", "Cilantro", "Perejil"]
+def read_file(file_name):
+    # Lee el archivo quitando los saltos de línea
+    with open(file_name, "r", encoding="utf-8") as file:
+        return file.read().splitlines()
 
+# Lee el contenido de los .txt
+fruit_list = read_file("Frutas.txt")
+vegetable_list = read_file("Vegetales.txt")
+
+# Se le pide al usuario ingresar una fruta o verdura
 word = input("Ingrese una fruta o vegetal: ")
 
+# Comprueba si la palabra esta en una de las dos listas
 if word in fruit_list:
-    print(f"La fruta {word} esta en la lista de frutas")
+    print(f"La fruta {word} está en la lista de frutas")
 elif word in vegetable_list:
-    print(f"El vegetal {word} esta en la lista de vegetales")
+    print(f"El vegetal {word} está en la lista de vegetales")
 else:
-    print(f"{word} no esta en ninguna de las dos listas")
-
+    print(f"{word} no esta en ninguna lista")
